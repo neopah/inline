65d29121fe315f9793ef7819abaceefa9294e79b
vsweet <yaniszahiruddin68@gmail.com>
2025-06-27 05:10:38 +0000

    SAVE

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -299,6 +299,7 @@ enum rw_hint {
 #define IOCB_SYNC		(1 << 5)
 #define IOCB_WRITE		(1 << 6)
 #define IOCB_NOWAIT		(1 << 7)
+#define IOCB_NOIO       (1 << 8)
 /* kiocb is a read or write operation submitted by fs/aio.c. */
 #define IOCB_AIO_RW		(1 << 23)
 
